7d3b9252e632145c39439cd688f43e78885ee079
Hamim <hebronhossainhamim@gmail.com>
2022-11-17 10:19:39 +0600

    [add] report and employee test cases

---
+++ README.md
@@ -0,0 +1,34 @@
+# Selenium Automation on Open Source OrangeHRM 
+
+## Technologies Used:
+  - Java
+  - Selenium (4.6)
+  - TestNG
+  - Allure
+  - Faker Library
+
+
+## Flow Of Automation
+  1. Visit the site: [Open Source OrangeHRM](https://opensource-demo.orangehrmlive.com/) .
+  2. Assert the dashboard.
+  3. Create 2 new employees.
+  4. Search the employees with their Id and assert that 2 employees are found.
+  5. Then login with the last employee credential.
+  6. Update some employee info (e.g Nationality, Blood Group, License Expiry Date).
+  7. Now go to my info page and assert the edited info.
+  8. Finally logout.
+
+## Test Cases For Automation
+ - [Google Drive](https://docs.google.com/spreadsheets/d/1rV5zmZRtifpRC2xvnloNuAk0ri_6O85yqIyaYjh6kY0/edit?usp=sharing)
+
+ ## Requirements
+  - Java needs to be installed in device
+
+ ## Video of Result
+  - [Youtube](www.youtube.com)
+
+## Allure Report
+
+![Allure Report Overview](/images/overview.png "Allure Report Overview")
+![Allure Report Suites](/images/suites.png "Allure Report Suites")
+![Allure Report Overview](/images/graphs.png "Allure Report Graph")
---
+++ src/test/java/testrunners/LoginTestRunner.java
@@ -0,0 +1,47 @@
+package testrunners;
+
+import pages.PIMPage;
+import pages.LoginPage;
+import setup.Setup;
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class LoginTestRunner extends Setup {
+    PIMPage dashboardPage;
+    LoginPage loginPage;
+
+    @Test(priority = 1, description = "Login with invalid password")
+    public void doFailedLogin() throws InterruptedException {
+        loginPage = new LoginPage(driver);
+        driver.get("https://opensource-demo.orangehrmlive.com");
+        loginPage.doLogin("Admin", "wrongPassword");
+        String expectedText = "Invalid credentials";
+        String actualText = driver.findElement(By.className("oxd-alert-content-text")).getText();
+        Assert.assertTrue(actualText.contains(expectedText));
+        Thread.sleep(2000);
+    }
+    @Test(priority = 2, description = "Login with valid username and valid password")
+    public void doLogin() throws InterruptedException {
+        loginPage = new LoginPage(driver);
+        driver.get("https://opensource-demo.orangehrmlive.com");
+        loginPage.doLogin("Admin", "admin123");
+        Thread.sleep(5000);
+        String actualUrl = driver.getCurrentUrl();
+        String expectedUrl = "dashboard";
+        Assert.assertTrue(actualUrl.contains(expectedUrl));
+    }
+
+    @Test(priority = 3, description = "Check if Profile Image exists")
+    public void ifProfileImageExist() {
+        boolean imageExist = driver.findElement(By.className("oxd-userdropdown-img")).isDisplayed();
+        Assert.assertTrue(imageExist);
+    }
+
+    @Test(priority = 4, description = "Logs Out")
+    public void doLogout() {
+        dashboardPage = new PIMPage(driver);
+        dashboardPage.profileImage.click();
+        dashboardPage.logoutLink.click();
+    }
+}
